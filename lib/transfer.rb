54b3bcf1326e1f322bca4a920552105db6d72724
Marie Burns <mcburns2222@gmail.com>
2018-12-27 01:06:13 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,10 +15,10 @@ class Transfer
   end
   
   def execute_transaction
-    if valid? ==true && @status != "completed"
+    if valid? ==true && @status != "complete"
       sender.balance -= amount
       receiver.balance += amount
-      @status = "completed"
+      @status = "complete"
     else
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
