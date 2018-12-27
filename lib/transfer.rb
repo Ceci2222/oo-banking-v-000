60424a7c7156e60573457ff2ce02d3e7da0ab8e8
Marie Burns <mcburns2222@gmail.com>
2018-12-27 00:32:52 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,7 +10,7 @@ class Transfer
   end
   
   def valid?
-    sender.bank_account.valid?  && receiver.bank_account.valid?
+    sender.valid?  && receiver.valid?
   end
   
   def execute_transaction
