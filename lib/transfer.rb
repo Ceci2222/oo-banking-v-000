a8c1f803c2456f7ea27bb29622f0ec2966716076
Marie Burns <mcburns2222@gmail.com>
2018-12-27 01:19:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
       sender.balance -= amount
       receiver.balance += amount
       @status = "complete"
-    elsif sender.balance < @amount 
+    elsif sender.balance < @amount || sender.valid? != true
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
     else
