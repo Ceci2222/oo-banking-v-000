c5efb565db11c7ab401fcf2c3507480719e37a42
Marie Burns <mcburns2222@gmail.com>
2018-12-27 00:22:19 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,10 +1,22 @@
 class Transfer
   attr_accessor :status
-  attr_reader :sender, :receiver, :money
+  attr_reader :sender, :receiver, :amount
   
-  def initialize (sender, money, receiver)
+  def initialize (sender, amount, receiver)
+    @sender = sender
+    @amount = amount
+    @receiver = receiver
     @status = "pending"
   end
   
+  def valid?
+    sender.bank_account.valid?  && receiver.bank_account.valid?
+  end
+  
+  def execute_transaction
+    if valid? == true
+      sender.bank_account.balance -= amount
+      receiver.bank_account.balance += amount
+      @status = 
   
 end
