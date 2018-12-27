5a08fd1b759ec6c6a6c7ab35a537eba25d9ebebf
Marie Burns <mcburns2222@gmail.com>
2018-12-27 00:08:46 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,5 +1,6 @@
 class BankAccount
-  attr_accessor :name, :balance
+  attr_accessor :status, :balance
+  attr_reader :name
 
   def initialize(name)
     @name = name
@@ -7,5 +8,21 @@ class BankAccount
     @status = open
   end
   
+  def deposit(money)
+    @balance += money
+  end
+
+  def display_balance
+    @balance
+  end
+  
+  def valid?
+    if @status == open && balance > 0
+    end
+  end
+  
+  def close_account
+    @status = closed
+  end
   
 end
